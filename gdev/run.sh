83a558c3795961477a9b9a9630de08011d8f999b
Hangchen Yu <hyu@cs.utexas.edu>
2017-03-04 15:02:17 -0600

    Add gdev benchmark test script

---
+++ gdev/run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OCLDIR=$DIR/user
+
+# 11 in total
+# heartwall does not work
+bm="idle loop madd mmul fmadd fmmul memcpy memcpy_pinned"
+
+OUTDIR=$DIR/results
+mkdir $OUTDIR &>/dev/null
+mkdir $OUTDIR/srad &>/dev/null
+
+cd $OCLDIR
+exe() { echo "++ $@" |& tee -a $OUTDIR/$b.txt ; \
+    $@ |& tee -a $OUTDIR/$b.txt ; }
+
+for b in $bm; do
+    echo -n > $OUTDIR/$b.txt # clean output file
+    echo "$(date) # running $b"
+    cd $b
+    make
+    for idx in `seq 1 10`; do
+        #exe sudo /usr/bin/time ./run
+        exe sudo ./user_test
+        exe echo
+	sleep 2
+    done
+    cd $OCLDIR
+    exe echo
+    echo
+done
