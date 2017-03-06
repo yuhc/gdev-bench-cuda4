f13848b023b7ae6325d21ac830fbd9ed8a9d0af8
yuhc <yuhc123@gmail.com>
2017-03-06 16:34:00 -0600

    Time backprop more precisely

--- rodinia/cuda/backprop/backprop_cuda.c
+++ rodinia/cuda/backprop/backprop_cuda.c
@@ -8,6 +8,23 @@
 #include "util.h"
 #include "backprop.h"
 
+struct timeval tv;
+struct timeval tv_total_start, tv_total_end;
+float total;
+struct timeval tv_h2d_start, tv_h2d_end;
+float h2d;
+struct timeval tv_d2h_start, tv_d2h_end;
+float d2h;
+struct timeval tv_exec_start, tv_exec_end;
+struct timeval tv_mem_alloc_start;
+struct timeval tv_close_start;
+struct timeval tv_idle_start, tv_idle_end;
+float mem_alloc;
+float exec;
+float init_gpu;
+float close_gpu;
+float idle;
+
 ////////////////////////////////////////////////////////////////////////////////
 
 void bpnn_layerforward(float *l1, float *l2, float **conn, int n1, int n2);
@@ -125,13 +142,13 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 	CUcontext ctx;
 	CUmodule mod;
 	CUresult res;
-	
+
 	input_weights_one_dim = 
 		(float *) malloc((in + 1) * (hid + 1) * sizeof(float));
 	input_weights_prev_one_dim = 
 		(float *) malloc((in + 1) * (hid + 1) * sizeof(float));
 	partial_sum = (float *) malloc(num_blocks * WIDTH * sizeof(float));
-	
+
 	/* this preprocessing stage is added to correct the bugs of wrong 
 	   memcopy using two-dimensional net->inputweights */
 	for (k = 0; k <= in; k++) {	
@@ -141,16 +158,21 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 			m++;
 		}
 	}
-	
+
 	/*
 	 * call our common CUDA initialization utility function.
 	 */
+	gettimeofday(&tv_total_start, NULL);
 	res = cuda_driver_api_init(&ctx, &mod, "./backprop.cubin");
 	if (res != CUDA_SUCCESS) {
 		printf("cuda_driver_api_init failed: res = %u\n", res);
 		return ;
 	}
-	
+
+    gettimeofday(&tv_mem_alloc_start, NULL);
+	tvsub(&tv_mem_alloc_start, &tv_total_start, &tv);
+	init_gpu = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
 	/*
 	 * allocate device memory space
 	 */
@@ -184,6 +206,10 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		printf("cuMemAlloc failed: res = %u\n", res);
 		return ;
 	}
+
+    gettimeofday(&tv_h2d_start, NULL);
+    tvsub(&tv_h2d_start, &tv_mem_alloc_start, &tv);
+	mem_alloc = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
 #endif
 
 #ifdef CPU
@@ -194,11 +220,6 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 #ifdef GPU
 	printf("Performing GPU computation\n");
     //printf("in= %d, hid = %d, numblocks = %d\n", in, hid, num_blocks);
-  
-	/*
-	 * measurement start!
-	 */
-	time_measure_start(&tv);
 
 	res = cuMemcpyHtoD(input_cuda, net->input_units, (in + 1) * sizeof(float));
 	if (res != CUDA_SUCCESS) {
@@ -211,6 +232,10 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		return ;
 	}
 
+    gettimeofday(&tv_h2d_end, NULL);
+    tvsub(&tv_h2d_end, &tv_h2d_start, &tv);
+    h2d = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
 	res = bpnn_layerforward_launch(mod, input_cuda, output_hidden_cuda,
 								   input_hidden_cuda, hidden_partial_sum,
 								   in, hid);
@@ -218,9 +243,9 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		printf("bpnn_layerforward failed: res = %u\n", res);
 		return ;
 	}
- 
+
 	cuCtxSynchronize();
-  
+
 #if 0
 	cudaError_t error = cudaGetLastError();
 	if (error != cudaSuccess) {
@@ -228,12 +253,18 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		exit(EXIT_FAILURE);
 	}
 #endif
-  
+
+    gettimeofday(&tv_exec_end, NULL);
+    tvsub(&tv_exec_end, &tv_h2d_end, &tv);
+    exec = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
 	res = cuMemcpyDtoH(partial_sum, hidden_partial_sum, num_blocks * WIDTH * sizeof(float));
 	if (res != CUDA_SUCCESS) {
 		printf("cuMemcpyDtoH(layerforward) failed: res = %u\n", res);
 		return ;
 	}
+
+    gettimeofday(&tv_idle_start, NULL);
 
 	for (j = 1; j <= hid; j++) {
 		sum = 0.0;
@@ -249,12 +280,14 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 	bpnn_output_error(net->output_delta, net->target, net->output_units, out, &out_err);
 	bpnn_hidden_error(net->hidden_delta, hid, net->output_delta, out, net->hidden_weights, net->hidden_units, &hid_err);  
 	bpnn_adjust_weights(net->output_delta, out, net->hidden_units, hid, net->hidden_weights, net->hidden_prev_weights);
-	
+
 #ifdef CPU
 	bpnn_adjust_weights(net->hidden_delta, hid, net->input_units, in, net->input_weights, net->input_prev_weights);
 #endif  
 
 #ifdef GPU
+    gettimeofday(&tv_idle_end, NULL);
+
 	res = cuMemcpyHtoD(hidden_delta_cuda, net->hidden_delta, (hid + 1) * sizeof(float));
 	if (res != CUDA_SUCCESS) {
 		printf("cuMemcpyHtoD failed: res = %u\n", res);
@@ -271,6 +304,10 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		return ;
 	}
 
+    gettimeofday(&tv_h2d_end, NULL);
+    tvsub(&tv_h2d_end, &tv_idle_end, &tv);
+    h2d += tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
 	res = bpnn_adjust_weights_launch(mod, hidden_delta_cuda, hid, 
 									 input_cuda, in, 
 									 input_hidden_cuda, 
@@ -279,6 +316,10 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		printf("bpnn_adjust_weights failed: res = %u\n", res);
 		return ;
 	}
+
+    gettimeofday(&tv_exec_end, NULL);
+    tvsub(&tv_exec_end, &tv_h2d_end, &tv);
+    exec += tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
 
 	res = cuMemcpyDtoH(net->input_units, input_cuda, (in + 1) * sizeof(float));
 	if (res != CUDA_SUCCESS) {
@@ -291,7 +332,11 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		printf("cuMemcpyDtoH(adjust_weights) failed: res = %u\n", res);
 		return ;
 	}
-    
+
+	gettimeofday(&tv_d2h_end, NULL);
+    tvsub(&tv_d2h_end, &tv_exec_end, &tv);
+	d2h += tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
 	cuMemFree(input_cuda);
 	cuMemFree(output_hidden_cuda);
 	cuMemFree(input_hidden_cuda);
@@ -309,7 +354,25 @@ void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
 		printf("cuda_driver_api_exit faild: res = %u\n", res);
 		return ;
 	}
-	
+
+	gettimeofday(&tv_total_end, NULL);
+	tvsub(&tv_total_end, &tv_d2h_end, &tv);
+	close_gpu = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
+	tvsub(&tv_idle_end, &tv_idle_start, &tv);
+	idle = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0;
+
+	tvsub(&tv_total_end, &tv_total_start, &tv);
+	total = tv.tv_sec * 1000.0 + (float) tv.tv_usec / 1000.0 - idle;
+
+	printf("Init: %f\n", init_gpu);
+	printf("MemAlloc: %f\n", mem_alloc);
+	printf("HtoD: %f\n", h2d);
+	printf("Exec: %f\n", exec);
+	printf("DtoH: %f\n", d2h);
+	printf("Close: %f\n", close_gpu);
+	printf("Total: %f\n", total);
+
 	free(partial_sum);
 	free(input_weights_one_dim);
 	free(input_weights_prev_one_dim);
